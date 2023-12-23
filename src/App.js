cae47b6c309e891b744debc13c188b0e4e6ba15e
aoi0412 <aoiaoi0412.1203@gmail.com>
2023-12-23 17:39:49 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useRef } from "react";
 
-function App() {
+const YoutubePartyApp = () => {
+  const [playing, setPlaying] = useState(false);
+  const youtubePlayerRef = useRef(null);
+
+  useEffect(() => {
+    // YouTube IFrame Player APIのロード
+    const tag = document.createElement("script");
+    tag.src = "https://www.youtube.com/iframe_api";
+    const firstScriptTag = document.getElementsByTagName("script")[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    window.onYouTubeIframeAPIReady = loadYoutubePlayer;
+  }, []);
+
+  const loadYoutubePlayer = () => {
+    youtubePlayerRef.current = new window.YT.Player("youtube-player", {
+      height: "360",
+      width: "640",
+      videoId: "YOUR_VIDEO_ID", // ここにYouTube動画IDを設定
+      events: {
+        onReady: onPlayerReady,
+      },
+    });
+  };
+
+  const onPlayerReady = (event) => {
+    // プレーヤーの準備ができたら、ここで何かアクションを行う
+  };
+
+  const handlePlay = () => {
+    const randomTime = Math.floor(Math.random() * 120) + 1; // 1から120秒のランダムな時間
+    setPlaying(true);
+    youtubePlayerRef.current.playVideo();
+
+    setTimeout(() => {
+      youtubePlayerRef.current.pauseVideo();
+      setPlaying(false);
+    }, randomTime * 1000); // ミリ秒に変換
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ opacity: 0 }} id="youtube-player"></div>
+      <button onClick={handlePlay} disabled={playing}>
+        {playing ? "再生中..." : "プレゼント交換開始！"}
+      </button>
     </div>
   );
-}
+};
 
-export default App;
+export default YoutubePartyApp;
